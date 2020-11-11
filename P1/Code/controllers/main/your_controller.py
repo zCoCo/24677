3771f7b5a878b78131887138a9c560ebacfc4785
zCoCo <connor.caxd@gmail.com>
2020-11-11 18:24:11 -0500

    Improved P1 Controller (post-submission, in prep for P2)

--- P1/Code/controllers/main/your_controller.py
+++ P1/Code/controllers/main/your_controller.py
@@ -5,6 +5,7 @@ import numpy as np
 from base_controller import BaseController
 from scipy import signal, linalg
 from util import *
+from warnings import warn
 
 # CustomController class (inherits from BaseController)
 class CustomController(BaseController):
@@ -22,9 +23,73 @@ class CustomController(BaseController):
         self.m = 1888.6
         self.g = 9.81
         
+        ####
+        # Vehicle Configuration Constants:
+        ####
+        self.F_max = 15737 # [N] Maximum vehicle thrust
+        self.delta_max = np.pi/6 # [rad] Maximum steering angle (symmetric about 0)
+        
+        self.decel_max = 1.1 # [m/s/s] Maximum capable longitudinal deceleration rate under any condition (empirical from coasting down)
+        self.accel_max = self.F_max / self.m # [m/s/s] Maximum vehicle acceleration
+        
+        ####
+        # Track Configuration Constants:
+        ####
+        self.track_length = 1290.385282704354 # [m] Total path length of the track
+        
+        ####
+        # Settings:
+        ####
+        self.target_time = 40 # [s] Target time to complete loop
+        self.max_cornering_speed = 8.2  # [m/s] Maximum Cornering Speed (empirical)
+        self.max_cornering_speed = min(self.max_cornering_speed, self.track_length/self.target_time)
+        vcm = self.max_cornering_speed # short hand
+        
+        # Velocity Waypoints (what speed should the car be going at key points along the track):
+        self.vel_waypoints = [
+            (0,0),
+            (2256,vcm),
+            (2453,vcm),
+            (3236,2*vcm),
+            (5217,2*vcm),
+            (5835,vcm),
+            #(6574,2*vcm),
+            (7799,vcm),
+            (8203,vcm/2.0)
+        ]
+        
+        ####
+        # Setup:
+        ####
         self.time = 0 # [s] Current time into trajectory execution
-        self.target_time = 300 # [s] Target time to complete loop.
-        self.track_length = 1290.385282704354; # [m] Total path length of the track
+        
+        # Velocity Trajectory (at each position point along the track, what's the target vehicle speed). Used to set xdot depending on track conditions.
+        self.vel_traj, time_traj, v_ratio = self.fastest_trap_vel_traj(
+            pos_traj = self.trajectory,
+            vavg = self.track_length / self.target_time,
+            amax = self.accel_max,
+            dmax = self.decel_max,
+            vel_waypoints = self.vel_waypoints
+        )
+        print(r"Requested Avg. Velocity is {}% of max speed for given profile waypoints.".format(int(100*v_ratio)))
+        
+        if False:
+            # Plot Velocity Trajectory:
+            import matplotlib.pyplot as plt
+            plt.figure()
+            plt.plot(np.arange(0,self.vel_traj.shape[0]), self.vel_traj)
+            plt.title('Velocity Trajectory Points')
+            plt.show()
+            
+            plt.figure()
+            plt.plot(np.arange(0,time_traj.shape[0]), time_traj)
+            plt.title('Time Trajectory Points')
+            plt.show()
+            
+            plt.figure()
+            plt.plot(time_traj, self.vel_traj)
+            plt.title('Velocity Trajectory')
+            plt.show()
         
         ####
         # Control Parameters:
@@ -36,12 +101,12 @@ class CustomController(BaseController):
         #T_long = (85824 + 0.4*(86720-85824))/1000 # time for long controller to reach 90% SS with step input
         Ku_long = 163500 # Lowest long gain causing rapid oscillation in speed under step input
         Tu_long = (0.096/2 + 0.032/2) # Period of that oscillation
-        Ku_lat = 0.60*0.61
+        Ku_lat = 0.60#*0.61
         Tu_lat = 6
         self.k_pid_longitudinal = np.asarray([0.60*Ku_long, 1.2*Ku_long/Tu_long, 3*Ku_long*Tu_long/40]).reshape((1,3)) #np.asarray([163500,0,0]).reshape((1,3))#np.asarray([1.2*T_long/L_long, 1.2*T_long/L_long/(2.0*L_long), 1.2*T_long/L_long*0.5*L_long]).reshape((1,3))#np.asarray([0.60*Ku, 1.2*Ku/Tu, 3*Ku*Tu/40]).reshape((1,3))
         self.k_pid_lateral = np.asarray([0.60*Ku_lat, 1.2*Ku_lat/Tu_lat, 3*Ku_lat*Tu_lat/40]).reshape((1,3))#np.asarray([500,0,0]).reshape((1,3))#np.asarray([1,0,0]).reshape((1,3))#
         
-        self.look_ahead_multiple = 2.5 # How many car lengths to look ahead and average over when determining desired heading angle
+        self.look_ahead_multiple = 7.5 # How many car lengths to look ahead and average over when determining desired heading angle
         self.look_ahead_indices = int(self.look_ahead_multiple * 24.0) # Corresponding number of indices (note: car length is approx. equiv. to path length over 24 points)
         
         ### Time of Last Zero Crossing for Each Signal (for determining Ziegler-Nichols Tu):
@@ -56,6 +121,190 @@ class CustomController(BaseController):
         # Rows are: (x,y,psi) = (alongtrack, crosstrack, heading),
         # Cols are: Position, Integral, Derivative
         self.E = np.zeros((3,3))
+        
+        self.decel_rate = 0
+        self.xdot_last = 0
+
+    def fastest_trap_vel_traj(self, pos_traj, vavg, amax, dmax, vel_waypoints):
+        """
+        Compute the fastest *max deceleration* velocity trajectory.
+        
+        NOTE: @TODO: This function is still very buggy (its computed 
+                     trajectories don't meet the required constraints but it 
+                     generally does something close to correct so for this 
+                     assignment it is sufficient).
+        
+        The fastest velocity trajectory is an array consisting of a target 
+        (longitudinal) velocity for each point in the given position trajectory 
+        while ensuring that the vehicle hits each target velocity at each of 
+        the specified indices for each of the (waypoint_idx, target_velocity) 
+        tuples in the vel_waypoints array.
+        
+        That is, between each waypoint specified in vel_waypoints, a 
+        path-length parameterized trapezoidal velocity profile is computed 
+        which starts at the previous target velocity at the previous index 
+        and ends at the specified target velocity at the specified index while 
+        ensuring that the vehicle *decelerates at dmax*, doesn't exceed 
+        amax (or throws a warning if it does), and ensures that the average 
+        velocity of the entire trajectory is vavg.
+        This is best used when you want to wait to brake until the last 
+        possible second (for drifting) or have very limited braking (i.e. you 
+        can only coast down from a speed and don't have real brakes).
+        
+        Returns the fastest *max deceleration* velocity trajectory as a numpy 
+        array as well as the ratio vavg to its maximum allowable value (the 
+        value above which the vehicle will have to exceed amax).
+        
+        Note:
+        - If the index of the first waypoint isn't 0, (0,0) will get added 
+        to the beginning.
+        - If the index of the last waypoint isn't pos_traj.shape[0], 
+        (pos_traj.shape[0], 0) will be added.
+        - If amax is ever exceeded, a warning will be thrown but the trajectory
+        will still be computed as normal (requiring the vehicle to exceed amax). 
+        This is useful if you want the vehicle to just floor it after a certain 
+        point by setting an unreachably high target velocity for the final waypoint.
+
+        Parameters
+        ----------
+        pos_traj
+            Nx2 numpy array containing the [x,y] coordinates of each waypoint.
+        vavg
+            Target average velocity for the entire trajectory (used for tuning vehicle performance).
+        amax
+            Maximum allowable vehicle acceleration.
+        dmax
+            Maximum achievable vehicle deceleration (used for all decelerations).
+        vel_waypoints
+            List of tuples containing the index of a waypoint and the target vehicle velocity for that waypoint.
+
+        Returns
+        -------
+        vel_traj
+            Computed velocity trajectory.
+        time_traj
+            The target time for arriving at each point in the trajectory.
+        vavg_ratio_max
+            Largest ratio of vavg to vavg_max. That is: how close is vavg to requiring one of the trapezoids to exceed amax
+
+        """
+        if vel_waypoints[0][0] != 0:
+            vel_waypoints = [(0,0)] + vel_waypoints
+        
+        if vel_waypoints[-1][0] != pos_traj.shape[0]:
+            vel_waypoints = vel_waypoints + [(pos_traj.shape[0],0)]
+        
+        traj_info = []
+        vavg_ratio_max = 0 # How close is vavg to requiring one of the trapezoids to exceed amax
+        for w in range(1,len(vel_waypoints)):
+            i_prev, v_prev = vel_waypoints[w-1]
+            i, vi = vel_waypoints[w]
+            
+            # Path length of section:
+            s = self.path_length(pos_traj, i_prev, i)
+            sd = s * dmax;
+            
+            # Peak velocity:
+            vp = (vi**2 - v_prev*vi + 2*sd - (sd*v_prev)/vavg)/(vi - v_prev + sd/vavg)
+            # Required acceleration:
+            a = (vavg*(vavg*v_prev**2 - 2*vavg*v_prev*vi - 2*sd*v_prev + vavg*vi**2 + 2*sd*vavg))/(s*(- 2*vavg**2 + 2*vi*vavg + sd))
+            
+            if abs(a) > amax:
+                warn("Max acceleration of {}m/s/s being exceeded at {}m/s/s between {} and {} in fastest_trap_vel_traj".format(amax, a, vel_waypoints[w-1], vel_waypoints[w]))
+            
+            # Maximum allowable average velocity between these two waypoints:
+            vavg_max = (sd*v_prev + np.sqrt((sd + amax*s)*(sd*v_prev**2 + amax*s*vi**2 + 2*amax*s*sd)) + amax*s*vi)/(v_prev**2 - 2*v_prev*vi + vi**2 + 2*sd + 2*amax*s)
+            
+            # Ratio of requested vavg to vavg_max:
+            vavg_ratio = vavg / vavg_max
+            # Store the highest one:
+            vavg_ratio_max = max(vavg_ratio, vavg_ratio_max)
+            
+            # Midpoint Distance:
+            sm = (vp**2 - v_prev**2) / 2 / a
+            # Endpoint Distance:
+            se = sm + (vp**2 - vi**2) / 2 / dmax
+            
+            # Append info defining the sub trapezoidal trajectory:
+            traj_info.append((vp,a,sm,se))
+        
+        print(vel_waypoints)
+        print(traj_info)
+        
+        
+        time_traj = np.zeros((pos_traj.shape[0],1))
+        vel_traj = np.zeros((pos_traj.shape[0],1))
+        vel_traj[0] = vel_waypoints[0][1]
+        
+        t = 0 # trajectory info index
+        s = 0 # total distance into trajectory of current waypoint
+        t0 = 0
+        s0 = 0 # starting distance of current trapezoid
+        vp,a,sm,se = traj_info[t] # parameters of current trapezoid
+        v_prev = vel_waypoints[t][1]
+        vi = vel_waypoints[t+1][1]
+        for i in range(1,pos_traj.shape[0]):
+            s = s + np.sqrt(np.sum((pos_traj[i] - pos_traj[i-1])**2))
+            
+            # Ensure point is still in the trapezoid:
+            while s > (se + s0):
+                print((t,s0,s,(se+s0)))
+                t = t+1
+                s0 = se + s0
+                t0 = (np.sqrt(v_prev**2 + 2*a*sm) - v_prev)/a + (vp - np.sqrt(vp**2 - 2*dmax*(se-sm)))/dmax + t0
+                vp,a,sm,se = traj_info[t]
+                v_prev = vel_waypoints[t][1]
+                vi = vel_waypoints[t+1][1]
+            
+            assert(s0 <= s <= (se+s0))
+            #assert(sm <= se)
+            
+            if s < (sm + s0):
+                # If in acceleration portion:
+                time_traj[i] = (np.sqrt(v_prev**2 + 2*a*(s-s0)) - v_prev)/a + t0
+                vel_traj[i] = np.sqrt(v_prev**2 + 2*a*(s-s0)) #v_prev + np.sqrt(2*a*(s-s0))
+            else:
+                # If in decelertion portion:
+                time_traj[i] = (np.sqrt(v_prev**2 + 2*a*sm) - v_prev)/a + (vp - np.sqrt(vp**2 - 2*dmax*(s-sm-s0)))/dmax + t0
+                vel_traj[i] = np.sqrt(vp**2 - 2*dmax*(s-s0-sm)) #vp - np.sqrt(2*dmax*(s-sm-s0))
+                
+        print(vel_traj)
+        return vel_traj, time_traj, vavg_ratio_max
+                
+ 
+    def path_length(self, pos_traj, start, end):
+        """
+        Compute the path distance between two points on a trajectory.
+        
+        Computes the path_length distance along the given position trajectory 
+        between the points at the given indices. If end < start, the distance 
+        will be negative.
+
+        Parameters
+        ----------
+        pos_traj
+            Nx2 numpy array containing the [x,y] coordinates of each waypoint.
+        start
+            Index of the first point.
+        end
+            Index of the second point.
+
+        Returns
+        -------
+        path_distance
+            Path length distance between points at given indices.
+
+        """
+        idx_1, idx_2 = tuple(map(int, (start, end)))
+        path_sign = 1.0
+        if idx_1 > idx_2:
+            # Ensure proper ordering. Swap order and sign if necessary.
+            path_sign = -1.0
+            idx_1, idx_2 = idx_2, idx_1
+        section = pos_traj[idx_1:(idx_2+1)] # Section of trajectory between idx_1 and idx_2, inclusive
+        point_distances = np.sqrt(np.sum(np.diff(section, axis=0)**2, axis=1)) # Distances between each point
+        path_distance = path_sign * np.sum(point_distances)
+        return path_distance
 
     def update(self, timestep):
         
@@ -96,7 +345,7 @@ class CustomController(BaseController):
         ### Compute Pose to Path Heading Angle Error:
         # Points that comes before and 2.5 car lengths later waypoint:
         nearest_waypoint_prev = trajectory[(nearest_waypoint_idx-1) % trajectory.shape[0]]
-        nearest_waypoint_fwd = trajectory[(nearest_waypoint_idx+self.look_ahead_indices) % trajectory.shape[0]] # look roughly 2.5 car lengths ahead
+        nearest_waypoint_fwd = trajectory[(nearest_waypoint_idx+self.look_ahead_indices) % trajectory.shape[0]]
         # Desired Trajectory Position Delta around Current Waypoint:
         nearest_waypoint_delta = nearest_waypoint_fwd - nearest_waypoint_prev
         # Compute Desired Heading:
@@ -111,7 +360,16 @@ class CustomController(BaseController):
         ####
         # Compute Alongtrack Error as longitudinal speed difference:
         ####
-        speed_diff = self.track_length / self.target_time - xdot
+        speed_diff = self.vel_traj[nearest_waypoint_idx] - xdot
+        
+        print((self.vel_traj[nearest_waypoint_idx], xdot))
+        
+        if False:
+            # Print Deceleration Rate:
+            decel = (xdot - self.xdot_last)/delT
+            if decel < 0:
+                print(decel)
+            self.xdot_last = xdot
         
         """
         # Second oldest alongtrack error:
@@ -194,9 +452,9 @@ class CustomController(BaseController):
         kth = 1/kt;
         V = np.abs(np.linalg.norm([xdot, ydot]));
         if V < 0.05: # Prevent from getting too large
-            ky = 2 / 0.05 / (kt**2); 
+            ky = 2 / 2/0.05 / (kt**2); 
         else:
-            ky = 2 / V / (kt**2); # Lessen impact of crosstrack error as velocity increases (to minimize wild swings)
+            ky = 2 / 2/V / (kt**2); # Lessen impact of crosstrack error as velocity increases (to minimize wild swings)
         
         ####
         # Create a PID Constants Matrix, K:
@@ -219,13 +477,17 @@ class CustomController(BaseController):
         # ---------------|Lateral Controller|-------------------------
         # Note: Lateral controller consists of two PID subcontrollers working togther to minimize heading error and crosstrack error.
         # Using a heading control component allows for improved handling of turns (since its trying to point the car in the right direction)
-        delta = Cy + Cth; # Allow crosstrack and heading errors to drive the steering angle
+        delta = clamp(Cy + Cth, -self.delta_max, self.delta_max); # Allow crosstrack and heading errors to drive the steering angle
         # Note: This approach is effectively equivalent to having one lateral pid controller which controls (ey/kt + eth/V/kt**2) but is just a cleaner representation
 
         # ---------------|Longitudinal Controller|-------------------------
-        F = np.linalg.norm([clamp(Cx,0,15737), Cy]); # Allow crosstrack and alongtrack errors to drive the throttle
+        F = np.abs(np.linalg.norm([clamp(Cx,0,self.F_max), Cy])) # Allow crosstrack and alongtrack errors to drive the throttle
+        F = clamp(F,0,self.F_max) - np.abs(delta) * 0.1 * self.F_max / self.delta_max # don't accelerate as much if you're actively turning the wheel
+        F = clamp(F,0,self.F_max) # Ensure appropriate bounds (main will otherwise treat negative F as positive)
         # Note: This approach is effectively equivalent to having one longitudinal pid controller which controls norm[ey/kt, eth/V/kt**2] but is just a cleaner representation
         # Todo: Slow down based on Cth? (slower when steering high to increase control authority)
+
+        #print((int(100*delta*6/np.pi), int(100*F/15737)) )
 
         # Return all states and calculated control inputs (F, delta)
         return X, Y, xdot, ydot, psi, psidot, F, delta
